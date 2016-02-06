bc97b9cf000df0f113882c5d971a7966f82f34b3
pkarira <pulkit5208>
2016-02-06 17:41:43 +0530

    ok2

--- src/com/example/lovecal/MainActivity.java
+++ src/com/example/lovecal/MainActivity.java
@@ -9,6 +9,8 @@ public class MainActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+
+//hiiiiiiiiiiiiiiiiiiiii
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
